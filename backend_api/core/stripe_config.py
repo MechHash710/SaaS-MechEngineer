6a1a78b9bc768f712fe0fd0f94d2dcccb428cf36
MechHash710 <randhyjah@gmail.com>
2026-04-10 21:24:08 -0300

    fix(deps): add pydantic-settings, python-dotenv; remove dotenv from stripe_config

--- backend_api/core/stripe_config.py
+++ backend_api/core/stripe_config.py
@@ -1,13 +1,9 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "sk_test_mock")
-STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET", "whsec_mock")
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY", "sk_test_mock")
+STRIPE_WEBHOOK_SECRET = os.environ.get("STRIPE_WEBHOOK_SECRET", "whsec_mock")
 
 PRICE_IDS = {
-    "pro": os.getenv("STRIPE_PRICE_PRO", "price_pro_mock_id"),
-    "enterprise": os.getenv("STRIPE_PRICE_ENTERPRISE", "price_enterprise_mock_id"),
+    "pro": os.environ.get("STRIPE_PRICE_PRO", "price_pro_mock_id"),
+    "enterprise": os.environ.get("STRIPE_PRICE_ENTERPRISE", "price_enterprise_mock_id"),
 }
